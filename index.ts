23db0cf0f33003891dc9ef158a4304a7749943e6
Arnaud Moncel <arnaud.moncel43@gmail.com>
2025-06-11 17:13:12 +0200

    chore introspection

--- index.ts
+++ index.ts
@@ -3,74 +3,62 @@ import type { Schema } from './typings';
 
 import 'dotenv/config';
 import { createAgent } from '@forestadmin/agent';
-import { createSqlDataSource } from '@forestadmin/datasource-sql';
+import { createSqlDataSource, introspect } from '@forestadmin/datasource-sql';
+import fs from 'fs';
 
-// This object allows to configure your Forest Admin panel
-const agent = createAgent<Schema>({
-  // Security tokens
-  authSecret: process.env.FOREST_AUTH_SECRET!,
-  envSecret: process.env.FOREST_ENV_SECRET!,
-  forestServerUrl: process.env.FOREST_SERVER_URL!,
-
-  // Make sure to set NODE_ENV to 'production' when you deploy your project
-  isProduction: process.env.NODE_ENV === 'production',
-
-  // Autocompletion of collection names and fields
-  typingsPath: './typings.ts',
-  typingsMaxDepth: 5,
-});
-
-// Connect your datasources
-// All options are documented at https://docs.forestadmin.com/developer-guide-agents-nodejs/data-sources/connection
-agent.addDataSource(
-  createSqlDataSource({
+(async () => {
+  // Options to connect to the db (see above).
+  const connectionOptions = {
     uri: process.env.DATABASE_URL,
     schema: process.env.DATABASE_SCHEMA,
     sslMode: process.env.DATABASE_SSL_MODE as SslMode,
-  }),
-);
-
-// Add customizations here.
-// For instance, you can code custom actions, charts, create new fields or relationships, load plugins.
-// As your project grows, you will need to split it into multiple files!
-//
-// Here is some code to get your started
-//
-// agent.customizeCollection('products', collection => {
-//   // Actions are documented here:
-//   // https://docs.forestadmin.com/developer-guide-agents-nodejs/agent-customization/actions
-//   collection.addAction('Order new batch from supplier', {
-//     scope: 'Single', // This action can be triggered product by product
-//     form: [{ label: 'Quantity', type: 'Number', isRequired: true }],
-//     execute: async (context, resultBuilder) => {
-//       const product = await context.getRecord(['id', 'name'])
-//       const quantity = context.formValues['Quantity'];
-
-//       // ... Perform work here ...
-
-//       return resultBuilder.success(`Your order for a batch of ${quantity} '${product.name}' was sent`);
-//     }
-//   });
-
-//   // Search customization is documented here:
-//   // https://docs.forestadmin.com/developer-guide-agents-nodejs/agent-customization/search
-//   collection.replaceSearch(searchString => {
-//     // user has most likely typed a product id, let's search only that column
-//     if (searchString.match(/^prdid[\d]{8}/$))
-//       return { field: 'id', operator: 'Equal', value: searchString };
-
-//     // Otherwise assume that user wants to search for a product by name
-//     return { field: 'name', operator: 'Contains', value: searchString };
-//   });
-// });
-
-// Expose an HTTP endpoint.
-agent.mountOnStandaloneServer(Number(process.env.APPLICATION_PORT));
-
-// Start the agent.
-agent.start().catch(error => {
-  console.error('\x1b[31merror:\x1b[0m Forest Admin agent failed to start\n');
-  console.error('');
-  console.error(error.stack);
-  process.exit(1);
-});
+  };
+  const introspectionFilePath = './my-database-introspection.json';
+  
+  let introspection;
+  try {
+    // The introspection is JSON serializable. You can store it in a file.
+    // Read it from the file if it exists.
+    introspection = JSON.parse(fs.readFileSync(introspectionFilePath, 'utf-8'));
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      // The file does not exist, we need to introspect the database.
+      introspection = await introspect(connectionOptions);
+      fs.writeFileSync(introspectionFilePath, JSON.stringify(introspection));
+    } else {
+      throw e;
+    }
+  }
+  
+  // This object allows to configure your Forest Admin panel
+  const agent = createAgent<Schema>({
+    // Security tokens
+    authSecret: process.env.FOREST_AUTH_SECRET!,
+    envSecret: process.env.FOREST_ENV_SECRET!,
+    forestServerUrl: process.env.FOREST_SERVER_URL!,
+  
+    // Make sure to set NODE_ENV to 'production' when you deploy your project
+    isProduction: process.env.NODE_ENV === 'production',
+  
+    // Autocompletion of collection names and fields
+    typingsPath: './typings.ts',
+    typingsMaxDepth: 5,
+  });
+  
+  // Connect your datasources
+  // All options are documented at https://docs.forestadmin.com/developer-guide-agents-nodejs/data-sources/connection
+  agent.addDataSource(
+    createSqlDataSource(connectionOptions, { introspection }),
+  );
+  
+  // Expose an HTTP endpoint.
+  agent.mountOnStandaloneServer(Number(process.env.APPLICATION_PORT));
+  
+  // Start the agent.
+  agent.start().catch(error => {
+    console.error('\x1b[31merror:\x1b[0m Forest Admin agent failed to start\n');
+    console.error('');
+    console.error(error.stack);
+    process.exit(1);
+  });
+})();
